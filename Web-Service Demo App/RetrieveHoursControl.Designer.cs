a4a7d18fca8dbc40389c14a358fb9fa6d2b14f2d
farhat97 <44099258+farhat97@users.noreply.github.com>
2019-06-23 20:51:48 -0600

    Data retrieval implemented

--- Web-Service Demo App/RetrieveHoursControl.Designer.cs
+++ Web-Service Demo App/RetrieveHoursControl.Designer.cs
@@ -28,45 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView
-            // 
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView.Location = new System.Drawing.Point(3, 15);
-            this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(533, 299);
-            this.dataGridView.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(3, 320);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // RetrieveHoursControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView);
             this.Name = "RetrieveHoursControl";
             this.Size = new System.Drawing.Size(539, 351);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView;
-        private System.Windows.Forms.Button button1;
     }
 }
--- Web-Service Demo App/RetrieveHoursControl.cs
+++ Web-Service Demo App/RetrieveHoursControl.cs
@@ -15,6 +15,12 @@ using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Web;
+
 namespace Web_Service_Demo_App
 {
     public partial class RetrieveHoursControl : UserControl
@@ -42,68 +48,134 @@ namespace Web_Service_Demo_App
         {
             InitializeComponent();
 
-            LoadDataTable();
+            PassComplexData();
         }
 
-        // function to retrieve data
-        public async Task Get()
+        //public void GetTest()
+        //{
+        //    string rawJSON;
+        //    using (var webClient = new WebClient())
+        //    {
+        //        // get string representation of JSON data
+        //        rawJSON = webClient.DownloadString("https://se465-webservice.firebaseio.com/.json");
+
+        //        // convert to a list of shift objects
+        //        //ShiftCollection shiftCollection = JsonConvert.DeserializeObject<ShiftCollection>(rawJSON);
+
+        //        // convert rawJSON into JSON object
+        //        JObject json = JObject.Parse(rawJSON);
+
+        //        // pass json object to web service
+
+
+        //        Console.WriteLine("Shift JSON: ");
+        //        Console.WriteLine(rawJSON);
+
+        //    }
+        //}
+
+        /*public async Task<string> GetTest()
         {
-            Control activeControl = this.FindForm();
-            IFirebaseClient firebaseClient = ((Form1)activeControl).GetFirebaseClient();
+            Console.WriteLine("In function");
+            string response = "";
 
-            //var result = await firebaseClient.GetTaskAsync("EmployeeShifts/");
-            FirebaseResponse response = await firebaseClient.GetTaskAsync("EmployeeShifts/");
-            ShiftData shiftData = response.ResultAs<ShiftData>();
+            var webClient = new WebClient();
 
-            /*while (shiftData != null)
+            // get string representation of JSON data
+            string rawJSON = webClient.DownloadString("https://se465-webservice.firebaseio.com/.json");
+
+            // convert rawJSON into JSON object
+            JObject json = JObject.Parse(rawJSON);
+
+            try
             {
-                DataRow row = dataTable.NewRow();
+                HttpResponseMessage httpResponseMessage = null;
 
-                row["ID"] = shiftData.Id;
-                row["Name"] = shiftData.Name;
-                row["Date"] = shiftData.Date;
-                row["Start Time"] = shiftData.startTime;
-                row["End Time"] = shiftData.endTime;
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                Console.WriteLine("Debug: " + shiftData.Id);
-            }*/
-        }
+                    var httpContent = new StringContent(rawJSON, Encoding.UTF8, "application/json");
 
-        public void GetTest()
-        {
-            using (var webClient = new WebClient())
-            {
-                // get string representation of JSON data
-                string rawJSON = webClient.DownloadString("https://se465-webservice.firebaseio.com/.json");
+                    httpResponseMessage = await httpClient.PostAsync("http://localhost:32160/api/home/GetShift", httpContent);
 
-                // convert to a list of shift objects
-                //ShiftCollection shiftCollection = JsonConvert.DeserializeObject<ShiftCollection>(rawJSON);
-
-                // convert rawJSON into JSON object
-                JObject json = JObject.Parse(rawJSON);
-
-                Console.WriteLine("Shift JSON: ");
-                Console.WriteLine(rawJSON);
-
+                    if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                    {
+                        response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+                    }
+                    else
+                    {
+                        response = "Some error occured." + httpResponseMessage.StatusCode;
+                    }
+                }
             }
-        }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error while passing info");
+            }
 
-        private void LoadDataTable()
+            return response;
+        }*/
+
+        public string PassComplexData()
         {
-            // populate data rows
-            dataTable.Columns.Add("ID");
-            dataTable.Columns.Add("Name");
-            dataTable.Columns.Add("Date");
-            dataTable.Columns.Add("Start Time");
-            dataTable.Columns.Add("End Time");
+            string responseString = "";
+            HttpWebResponse response = null;
 
-            dataGridView.DataSource = dataTable;
-        }
+            var webClient = new WebClient();
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //Get();
-            GetTest();
+            // get string representation of JSON data
+            string rawJSON = webClient.DownloadString("https://se465-webservice.firebaseio.com/.json");
+
+            Console.WriteLine(rawJSON);
+
+            // convert rawJSON into JSON object
+            JObject json = JObject.Parse(rawJSON);
+
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create("http://localhost:64085/api/home/GetShift");
+
+                request.Accept = "application/json";
+                request.Method = "POST";
+
+                ShiftData shift = new ShiftData()
+                {
+                    Id = "165432",
+                    Name = "Mehdi",
+                    Date = "June 3",
+                    StartTime = "2:00 pm",
+                    EndTime = "3:00 pm"
+                };
+
+                var data = Encoding.ASCII.GetBytes(shift.ToString());
+
+                request.ContentType = "application/json";
+                request.ContentLength = data.Length;
+
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+
+                response = (HttpWebResponse)request.GetResponse();
+
+                responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError)
+                {
+                    response = (HttpWebResponse)e.Response;
+                    responseString = "Some error occured: " + response.StatusCode.ToString();
+                }
+                else
+                {
+                    responseString = "Some error occured: " + e.Status.ToString();
+                }
+            }
+
+            return responseString;
         }
     }
 }
